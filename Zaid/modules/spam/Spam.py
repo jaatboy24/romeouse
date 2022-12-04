d0f700a57db3a12b9aec38ef08e141dc0290a12f
Rj-sgshsb <119700345+Rj-sgshsb@users.noreply.github.com>
2022-12-04 15:42:43 +0530

    Update Spam.py

--- Zaid/modules/spam/Spam.py
+++ Zaid/modules/spam/Spam.py
@@ -40,6 +40,32 @@ async def extract_args(message, markdown=True):
     text = text[text.find(" ") :].strip()
     return text
 
+@Client.on_message(filters.command("dspam") & filters.me)
+async def delayspam_handler(app: Client, m: Message):
+    try:
+        reply = m.reply_to_message
+        m.command
+ 
+        if app.long(m) < 3:
+            await app.send_edit(
+                m,
+                f"use as: .delayspam [conut] [ time in sec ] [text]",
+            )
+ 
+        elif app.long(m) > 2 and not reply:
+            await m.delete()
+            msg = m.text.split(None, 3)
+            times = int(msg[1]) if msg[1].isdigit() else None
+            sec = int(msg[2]) if msg[2].isdigit() else None
+            text = msg[3]
+            for x in range(times):
+                await app.send_message(m.chat.id, text)
+                await asyncio.sleep(sec)
+        else:
+            await app.send_edit(m, "something wrong")
+    except Exception as e:
+        await app.error(m, e)
+
 
 @Client.on_message(
     filters.command(commands, ".") & (filters.me | filters.user(SUDO_USER))
@@ -110,5 +136,9 @@ add_command_help(
     "spam",
     [
         ["spam <amount of spam> <text>", "Spamming texts in chats!!"],
+        [
+            "delayspam <seconds> <amount of spam> <text>",
+            "Send spam text with a specified delay period!",
+        ],
     ],
 )
