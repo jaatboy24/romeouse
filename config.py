27e5ef5640b825f7bcd7ed028cfa0ab8f13d0416
Rj-sgshsb <119700345+Rj-sgshsb@users.noreply.github.com>
2022-12-04 13:47:07 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,27 @@
+from os import getenv
+
+API_ID = int(getenv("API_ID", "15350244")) #optional
+API_HASH = getenv("API_HASH", "28100f5f7ebbc51d16ca759c87af30dc") #optional
+
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5368154755").split()))
+OWNER_ID = int(getenv("OWNER_ID", "1886471878"))
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://Oyehoye143:Oyehoye143@cluster0.eth7a3p.mongodb.net/?retryWrites=true&w=majority")
+BOT_TOKEN = getenv("BOT_TOKEN", "5423617634:AAGilf7a2Od1S7mK9ozNIl6VkN1q9zXcAZE")
+ALIVE_PIC = getenv("ALIVE_PIC", "https://telegra.ph/file/a62b9c7d9848afde0569e.jpg")
+ALIVE_TEXT = getenv("ALIVE_TEXT", "ʀᴏᴍᴇᴏʙᴏᴛ")
+PM_LOGGER = getenv("PM_LOGGER", "-1001882915379")
+LOG_GROUP = getenv("LOG_GROUP", "-1001882915379")
+GIT_TOKEN = getenv("GIT_TOKEN", "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") #personal access token
+REPO_URL = getenv("REPO_URL", "https://github.com/Rj-sgshsb/ZAID-USERBOT")
+BRANCH = getenv("BRANCH", "master") #don't change
+ 
+STRING_SESSION1 = getenv("STRING_SESSION1", "BQBi0s3lEwMxWHG78xM1QTnchztEoKw1l1ns-yswuR1sW7_fzWmn9BFEeCLqKfhaC-_722nVSPV1H_AxTTuwRoXFeNAdb-PFBszcAvKyIohoTnxg8G5CS1iaDxg2L5JMUBlPdZqlDI_oy6FzBHWfb6SOr9Mtb5zBtOLBqKYEcMEY5Z6lO_nGgVT6gahy78GosPp5czTBdrG-AcJj4LTGjsHU1xiZC1L6B-av14T7UsgYltEwmV4UShh_NqYK400rfWKFUg7EkA-AHUlxfNbJ4bccmnZ0phQyKz2Gyn15jhmdFWHnna94wDvWsIzRErsTYE-V3Z7g5p6JI3HQuGyqT3U1cHFGxgA")
+STRING_SESSION2 = getenv("STRING_SESSION2", "")
+STRING_SESSION3 = getenv("STRING_SESSION3", "")
+STRING_SESSION4 = getenv("STRING_SESSION4", "")
+STRING_SESSION5 = getenv("STRING_SESSION5", "")
+STRING_SESSION6 = getenv("STRING_SESSION6", "")
+STRING_SESSION7 = getenv("STRING_SESSION7", "")
+STRING_SESSION8 = getenv("STRING_SESSION8", "")
+STRING_SESSION9 = getenv("STRING_SESSION9", "")
+STRING_SESSION10 = getenv("STRING_SESSION10", "")
